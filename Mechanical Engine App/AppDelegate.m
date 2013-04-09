675c9158a32948804fcbd3d2d4f05184b36d2741
MrNat <nathan.hunt@comcast.net>
2013-04-08 22:30:02 -0400

    Initial Commit

---
+++ Mechanical Engine App/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Mechanical Engine App
+//
+//  Created by Nathan Hunt on 4/8/13.
+//  Copyright (c) 2013 Bipolaron. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ Mechanical Engine App/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mechanical Engine App
+//
+//  Created by Nathan Hunt on 4/8/13.
+//  Copyright (c) 2013 Bipolaron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
